dc862a24894575de3682879cca42c579040d3bd1
Antony <amwaniki180@gmail.com>
2019-06-17 10:51:00 +0300

    generate password

--- run.py
+++ run.py
@@ -19,3 +19,7 @@ def verify_user(first_name,password):
 	'''
     checking_user = Credential.check_user(first_name,password)
 	return checking_user
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
