5c9a6ad6193bac348ad6e45dfc598302e823bef9
Antony <amwaniki180@gmail.com>
2019-06-17 12:33:04 +0300

    functionalised when one click ep,gp and ex incase broken indicate wrong option entered

--- run.py
+++ run.py
@@ -89,6 +89,17 @@ def main():
 								print(" ")
 								password = input('Enter your password: ').strip()
 								break
+                            elif psw_choice == 'gp':
+								password = generate_password()
+								break
+							elif psw_choice == 'ex':
+								break
+                            else:
+								print('sorry! Wrong option entered. Try again.')
+						save_credential(create_credential(user_name,site_name,account_name,password))
+						print(' ')
+						print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+						print(' ')
                         
     
 
